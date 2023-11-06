d779d63e85ed96e75be27a64e407b84b307bed81
Afsara Tasnim <58877506+afsara-tasnim@users.noreply.github.com>
2023-11-06 14:55:02 +0600

    Update sample_project.py
    second commit

--- sample_project.py
+++ sample_project.py
@@ -2,3 +2,8 @@ import os
 list1 = [12,23,12,42,12,33]
 list1.reverse()
 print("The reverse list is", list1)
+
+list2 = [2, 12, 3, 12, 111]
+list2 = [12,23,12,42,12,33]
+list2.reverse()
+print("The reverse list is", list2)
